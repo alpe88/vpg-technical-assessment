648501342fa478d06a02b1288cfbc43209a178f4
aleksandar <alpe88.dev@gmail.com>
2023-09-11 17:28:03 -0700

    added standard deviation function

--- Python_Program/Python_Program.py
+++ Python_Program/Python_Program.py
@@ -125,6 +125,28 @@ def MaximumTemperature(startDate, endDate):
         close_connection(connection)
 
 
+def StandardDeviation(startDate, endDate):
+    try:
+        connection = open_connection()
+        cursor = connection.cursor()
+
+        query_select = """SELECT temp FROM TempReadings WHERE date >= %s AND date <= %s"""
+        cursor.execute(query_select, (startDate, endDate))
+
+        temperature_readings = [row[0] for row in cursor.fetchall()]
+
+        if not temperature_readings:
+            print("No temperature readings found in the specified date range.")
+            return None
+
+        return calculate_standard_deviation(temperature_readings)
+    except Exception as e:
+        traceback.print_exc()
+        print(f"An error occurred: {e}")
+    finally:
+        close_connection(connection)
+
+
 start = datetime.now() - timedelta(days=2)
 end = datetime.now()
 
--- Python_Program/utils.py
+++ Python_Program/utils.py
@@ -13,3 +13,11 @@ def find_median(temperatures):
         middle_left = temperatures[(n // 2) - 1]
         middle_right = temperatures[n // 2]
         return (middle_left + middle_right) / 2.0
+
+
+def calculate_standard_deviation(temperatures):
+    mean = calculate_mean(temperatures)
+    squared_differences = [(x - mean) ** 2 for x in temperatures]
+    variance = sum(squared_differences) / len(temperatures)
+    standard_deviation = variance ** 0.5
+    return standard_deviation
