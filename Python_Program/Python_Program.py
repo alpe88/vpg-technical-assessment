ee59483d48cc4a054e7a8ddb95fd16ba86905609
aleksandar <alpe88.dev@gmail.com>
2023-09-11 17:22:54 -0700

    added mean calculation

--- Python_Program/Python_Program.py
+++ Python_Program/Python_Program.py
@@ -1,7 +1,9 @@
-from database import open_connection, close_connection
-from datetime import datetime, timedelta
-
 import traceback
+
+from Python_Program.database import open_connection, close_connection
+from Python_Program.utils import calculate_mean
+
+from datetime import datetime, timedelta
 
 MAX_TEMP = 0xFFFF
 
@@ -33,6 +35,28 @@ def ReadTemperatures(startDate, endDate):
         close_connection(connection)
 
     return readings
+
+
+def MeanTemperature(startDate, endDate):
+    try:
+        connection = open_connection()
+        cursor = connection.cursor()
+
+        query_select = """SELECT temp FROM TempReadings WHERE date >= %s AND date <= %s"""
+        cursor.execute(query_select, (startDate, endDate))
+
+        temperature_readings = [row[0] for row in cursor.fetchall()]
+
+        if not temperature_readings:
+            print("No temperature readings found in the specified date range.")
+            return None
+
+        return calculate_mean(temperature_readings)
+    except Exception as e:
+        traceback.print_exc()
+        print(f"An error occurred: {e}")
+    finally:
+        close_connection(connection)
 
 
 start = datetime.now() - timedelta(days=2)
---
+++ Python_Program/utils.py
@@ -0,0 +1,2 @@
+def calculate_mean(temperatures):
+    return sum(temperatures)/len(temperatures)
