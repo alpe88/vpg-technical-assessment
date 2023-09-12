23e8d96559ce5ec38a4eee060e4ef5b5fe865804
aleksandar <alpe88.dev@gmail.com>
2023-09-11 17:26:24 -0700

    added median function

--- Python_Program/Python_Program.py
+++ Python_Program/Python_Program.py
@@ -59,6 +59,28 @@ def MeanTemperature(startDate, endDate):
         close_connection(connection)
 
 
+def MedianTemperature(startDate, endDate):
+    try:
+        connection = open_connection()
+        cursor = connection.cursor()
+
+        query_select = """SELECT temp FROM TempReadings WHERE date >= %s AND date <= %s"""
+        cursor.execute(query_select, (startDate, endDate))
+
+        temperature_readings = [row[0] for row in cursor.fetchall()]
+
+        if not temperature_readings:
+            print("No temperature readings found in the specified date range.")
+            return None
+
+        return find_median(temperature_readings)
+    except Exception as e:
+        traceback.print_exc()
+        print(f"An error occurred: {e}")
+    finally:
+        close_connection(connection)
+
+
 start = datetime.now() - timedelta(days=2)
 end = datetime.now()
 
--- Python_Program/utils.py
+++ Python_Program/utils.py
@@ -1,2 +1,15 @@
 def calculate_mean(temperatures):
     return sum(temperatures)/len(temperatures)
+
+
+def find_median(temperatures):
+    temperatures.sort()
+    n = len(temperatures)
+
+    # The median number is either the middle element of an odd length array or the average of the middle two when length is even
+    if n % 2 == 1:
+        return temperatures[n // 2]
+    else:
+        middle_left = temperatures[(n // 2) - 1]
+        middle_right = temperatures[n // 2]
+        return (middle_left + middle_right) / 2.0
