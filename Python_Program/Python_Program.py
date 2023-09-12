2b9cbb13e6788263969a13f71f0da59b50e4ef69
aleksandar <alpe88.dev@gmail.com>
2023-09-11 17:27:16 -0700

    added min/max functions

--- Python_Program/Python_Program.py
+++ Python_Program/Python_Program.py
@@ -81,6 +81,50 @@ def MedianTemperature(startDate, endDate):
         close_connection(connection)
 
 
+def MinimumTemperature(startDate, endDate):
+    try:
+        connection = open_connection()
+        cursor = connection.cursor()
+
+        query_select = """SELECT temp FROM TempReadings WHERE date >= %s AND date <= %s"""
+        cursor.execute(query_select, (startDate, endDate))
+
+        temperature_readings = [row[0] for row in cursor.fetchall()]
+
+        if not temperature_readings:
+            print("No temperature readings found in the specified date range.")
+            return None
+
+        return min(temperature_readings)
+    except Exception as e:
+        traceback.print_exc()
+        print(f"An error occurred: {e}")
+    finally:
+        close_connection(connection)
+
+
+def MaximumTemperature(startDate, endDate):
+    try:
+        connection = open_connection()
+        cursor = connection.cursor()
+
+        query_select = """SELECT temp FROM TempReadings WHERE date >= %s AND date <= %s"""
+        cursor.execute(query_select, (startDate, endDate))
+
+        temperature_readings = [row[0] for row in cursor.fetchall()]
+
+        if not temperature_readings:
+            print("No temperature readings found in the specified date range.")
+            return None
+
+        return max(temperature_readings)
+    except Exception as e:
+        traceback.print_exc()
+        print(f"An error occurred: {e}")
+    finally:
+        close_connection(connection)
+
+
 start = datetime.now() - timedelta(days=2)
 end = datetime.now()
 
